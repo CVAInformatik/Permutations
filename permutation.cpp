fedb34809807c7b8910fd66ce37211dd17b9e5b4
CVAInformatik <159441700+CVAInformatik@users.noreply.github.com>
2024-09-19 16:58:45 +0200

    Larger pemutation feature added

--- permutation.cpp
+++ permutation.cpp
@@ -10,8 +10,77 @@ typedef unsigned long int u64;
 #define LIMIT  362880
 
 
+typedef unsigned int SomeType;
 
-void mkAnagram1(const u64 N, const std::string& inp, std::string & outp)
+
+
+void mkPermutation(const u64 N, const std::vector<SomeType>  & inp, std::vector<SomeType> & outp)
+{
+    std::vector<SomeType> temp;
+    u64  Ntemp = N;
+    uint sz = inp.size();
+    uint outindex = 0;
+
+    if (sz > 20) sz = 20;
+
+    outp.clear();
+    switch (sz)
+    {
+    case 0: break;
+
+    case 1: outp.push_back(inp[0]); break;
+
+    default:
+        outp.push_back(inp[0]);
+        temp.clear();
+        outindex = 1;
+        do {
+            u64 s = (Ntemp % (outp.size() + 1));
+            Ntemp = Ntemp / (outp.size() + 1);
+            uint tsize = outp.size();
+            for (int r = 0; r < tsize; r++) {
+                if (r == s) { temp.push_back(inp[outindex]); }
+                temp.push_back(outp[r]);
+            }
+            if (temp.size() == tsize) temp.push_back(inp[outindex]);
+            outp = temp;
+            outindex++;
+            temp.clear();
+        } while (outp.size() < sz);
+        break;
+    }
+}
+
+#define MAXSIMPLE 20
+void mkLargePermutation(const u64 N, const std::vector<SomeType>& inp, std::vector<SomeType>& outp, uint stride = 10)
+{
+    std::vector<SomeType> temp = inp;
+    std::vector<SomeType> temp1 = inp;
+    std::vector<SomeType> buffer;
+    u64  N1 = N;
+
+    uint m = temp.size();
+    if (m > MAXSIMPLE) {
+
+        for (int i = 0; i < (4 * ((temp.size() / stride) + 1)); i++) {
+            mkPermutation(N1, temp, buffer);
+            for (int j = 0; j < buffer.size();j++)  temp[j] = buffer[j];
+            uint o = 0;
+            for (int k = stride; k < temp.size(); k++) temp1[o++] = temp[k];
+            for (int k = 0; k < stride; k++) temp1[o++] = temp[k];
+            temp = temp1;
+            N1 = N1 + stride;
+        }
+        outp = temp;
+    }
+    else
+        mkPermutation(N1, inp, outp);
+}
+
+
+
+
+void mkAnagram(const u64 N, const std::string& inp, std::string & outp)
 {
     std::string temp;
     u64  Ntemp = N;
@@ -50,12 +119,45 @@ void mkAnagram1(const u64 N, const std::string& inp, std::string & outp)
 
 }
 
+/*
+
+makes an anagram of string longer than 20 chars, by a set of overlapping anagrams of substrings.
+
+*/
+
+#define MAXSIMPLE 20
+void mkLargeAnagram(const u64 N, const std::string& inp, std::string& outp, uint stride = 10)
+{
+    std::string temp = inp;
+    std::string temp1 = inp;
+    std::string buffer ;
+    u64  N1 = N;
+
+    uint m = temp.size();
+    if (m > MAXSIMPLE) {
+
+        for (int i = 0; i < (4*((temp.size() / stride) + 1)); i++) {
+            mkAnagram(N1, temp, buffer);
+            for (int j = 0; j < buffer.size();j++)  temp[j] = buffer[j];
+            uint o = 0;
+            for (int k = stride; k < temp.size(); k++) temp1[o++] = temp[k];
+            for (int k = 0; k < stride; k++) temp1[o++] = temp[k];
+            temp = temp1;
+            N1 = N1 + stride;
+        }
+        outp = temp;
+    }
+    else             
+        mkAnagram(N1, inp, outp);
+}
+
+
 void test( u64 count, std::string s )
 {
     std::string res;
     for  (u64 i = 0; i < count; i++)  
     {
-        mkAnagram1(i, s, res);
+        mkAnagram(i, s, res);
         printf( "%9lu  %s \n", i, res.c_str());
 		}
 }
@@ -63,7 +165,7 @@ void test( u64 count, std::string s )
 void test1( u64 no, std::string s )
 {
     std::string res;
-    mkAnagram1(no, s, res);
+    mkLargeAnagram(no, s, res);
     printf( "%9lu  %s \n", no, res.c_str());
 }
 
@@ -77,6 +179,10 @@ int main()
 
    std::string c11("abcdefghijk");
    std::string c20("abcdefghijklmnoprstu");
+   std::string c36("abcdefghijklmnoprstuvwxyz0123456789A");
+
+   test1(((u64)9223372036854775807 + (u64)9223372036854775800), c36);
+   test1((u64)9223372036854775807 , c36);
 
    test1(19067857, c11);
    test1(19087857, c11);
