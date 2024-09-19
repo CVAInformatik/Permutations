38cdf016b124b715d1b055929ae798a807518eeb
CVAInformatik <159441700+CVAInformatik@users.noreply.github.com>
2024-09-19 12:35:40 +0200

    Completely reworked
    for i = 0 -> to n! it now produces n1 permuatiions of a n-length striing  , n < 21..

--- permutation.cpp
+++ permutation.cpp
@@ -4,62 +4,105 @@
 #include <vector>
 
 typedef unsigned int uint;
-/* 
+typedef unsigned long int u64;
 
-a is a string of N letters
-spec a vector of m numbers, interpreted modulo N, specifying a permutation
-  a spec of the form n1 n2 n3 ...nX...., means 
-           perform a n1 position cyclic right shift followed by  a swap of the first two elements followed by
-		       perform a n2 position cyclic right shift followed by  a swap of the first two elements followed by
 
-   if n is 0 the only thing happening is a swap of the first two elements
-   a spec of the form n1 0 n2  can be simplified to  ( (n1+n2) mod N), so 0 only makes sense as the first or last letter in a spec.
+#define LIMIT  362880
 
-ana is the result
 
-*/
-void mkAnagram(const std::vector<uint> spec, const std::string a, std::string & ana) {
 
-	std::string output = a;
-	
-	if ((a.size() > 0) && (spec.size() > 0)){
+void mkAnagram1(const u64 N, const std::string& inp, std::string & outp)
+{
+    std::string temp;
+    u64  Ntemp = N;
+    uint sz = inp.size();
+    uint outindex = 0;
 
-		int outpoint = 0;
-		for (int i = 0; i < spec.size(); i++) {
-			outpoint = (outpoint - spec[i]);
-			if(outpoint <0 ) outpoint += a.size();
-			int outpoint1 = (outpoint + 1) % a.size();
-			char tmp = output[outpoint];
-			output[outpoint] = output[outpoint1];
-			output[outpoint1] = tmp;
-		}
-		ana.clear();
-		for (int i = 0; i < a.size(); i++) 	ana.push_back(output[ (outpoint + i) % a.size()]);
+    if (sz > 20) sz = 20;
 
-	}
+    outp.clear();
+    switch (sz)
+    {
+    case 0: break;
+
+    case 1: outp.push_back(inp[0]); break;
+
+    default: 
+			outp.push_back(inp[0]);
+			temp.clear();
+			outindex = 1;
+			do {
+				u64 s = (Ntemp % (outp.size() + 1));
+				Ntemp = Ntemp / (outp.size() + 1);
+				uint tsize = outp.size();
+				for (int r = 0; r < tsize; r++) {
+					if (r == s) { temp.push_back(inp[outindex]); }
+					temp.push_back(outp[r]);
+				}
+				if (temp.size() == tsize) temp.push_back(inp[outindex]);
+
+				outp = temp;
+				outindex++;
+				temp.clear();
+			} while (outp.size() < sz);
+		break;
+    }
+
 }
 
-#define LIMIT 1000000
+void test( u64 count, std::string s )
+{
+    std::string res;
+    for  (u64 i = 0; i < count; i++)  
+    {
+        mkAnagram1(i, s, res);
+        printf( "%9lu  %s \n", i, res.c_str());
+		}
+}
 
+void test1( u64 no, std::string s )
+{
+    std::string res;
+    mkAnagram1(no, s, res);
+    printf( "%9lu  %s \n", no, res.c_str());
+}
 
 int main()
 {
-    std::string res;
-    std::string c("abcdefghijklmnop");
-    std::vector<uint> spec1 = { 0 };
-    for (int i = 0; i < LIMIT; i++)
-    {
-        uint carry = 1;
 
-        mkAnagram(spec1, c, res);;
-        std::cout << i << "  " << c << "   " << res << std::endl;
+   std::string c0("");
+   std::string c1("a");
+   std::string c9("abcdefghi");
+   std::string c10("abcdefghij");
 
-        for (int d = 0; d < spec1.size(); d++)
-        {
-            spec1[d] = (spec1[d] + carry) % c.size();
-            if (spec1[d] == 0) carry = 1;
-            else carry = 0;
-        }
-        if (carry) spec1.push_back(1);
-    }
+   std::string c11("abcdefghijk");
+   std::string c20("abcdefghijklmnoprstu");
+
+   test1(19067857, c11);
+   test1(19087857, c11);
+   test1( 9167857, c11);
+   test1(519067857, c20);
+   test1(419087857, c20);
+   test1(39167857, c20);
+   test1(1519067857, c20);
+   test1(1419087857, c20);
+   test1(139167857, c20);
+   test1(21519067857, c20);
+   test1(31419087857, c20);
+   test1(((unsigned long int )9223372036854775807 + (unsigned long int ) 9223372036854775800), c20);
+   test1(9223372036854775807, c20);
+   test1(8223372036854775807, c20);
+   test1(7223372036854775807, c20);
+   test1(6223372036854775807, c20);
+   test1(5223372036854775807, c20);
+   test1(4223372036854775807, c20);
+   test1(3223372036854775807, c20);
+   test1(2223372036854775807, c20);
+   test1(1223372036854775807, c20);
+   test1(471586864139167857, c20);
+   test1(371586864139167857, c20);
+   test1(271586864139167857, c20);
+   test1(171586864139167857, c20);
+   //test(11*10*LIMIT,c11);
+
 }
